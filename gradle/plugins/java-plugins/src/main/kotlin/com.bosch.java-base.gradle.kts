1cce3d66781d76813457062ddccc55ebcdd2b33e
Jendrik Johannes <jendrik.johannes@gmail.com>
2022-07-18 15:00:42 +0200

    Tasks

--- gradle/plugins/java-plugins/src/main/kotlin/com.bosch.java-base.gradle.kts
+++ gradle/plugins/java-plugins/src/main/kotlin/com.bosch.java-base.gradle.kts
@@ -7,6 +7,11 @@ java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
-tasks.compileJava {
-
+tasks.compileJava { // == tasks.named<JavaCompile>("compileJava")
+    options.encoding = "UTF-8"
 }
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}
+
