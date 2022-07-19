56075b353c8ae023249612940c56124bec296b01
Jendrik Johannes <jendrik.johannes@gmail.com>
2022-07-19 08:51:37 +0200

    Enable spring-boot-devtools for hot-reload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,5 +12,10 @@ dependencies {
 
     implementation(libs.spring.boot)
 
+    developmentOnly("org.springframework.boot:spring-boot-devtools:2.7.1")
+    // To use hot reload you typically run two Gradle processes in parallel (possible triggered through IntelliJ)
+    //   :app:bootRun  <- This keeps running and will hot-reload classes when changed
+    //   :app:compileJava <- Compile code after changes
+
     // implementation(libs.commons.lang)
 }
